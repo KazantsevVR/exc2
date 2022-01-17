9e70bfb28998593f30e8afc864b0bc407b4e9f2f
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Vitya", "SVO", (byte) 19);
+        userService.saveUser("Kirill", "nosov", (byte) 22);
+        userService.saveUser("Mark", "Uglov", (byte) 19);
+        userService.saveUser("Artem", "Sobaka", (byte) 19);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
