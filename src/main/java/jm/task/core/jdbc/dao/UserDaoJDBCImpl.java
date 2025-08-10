3815fee8a1f6e1b88d2551069adbbc8bc8e81732
kirl <kirl@ya.ru>
2025-08-10 17:06:55 +0300

    f

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,102 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.ResultSet.*;
+
+public class UserDaoJDBCImpl extends Util implements UserDao  {
+    public UserDaoJDBCImpl() {
+
+    }
+
+    private Connection connection;
+
+    {
+        connection = getConnection();
+    }
+//    Connection connection = null;
+
+    public void createUsersTable() throws SQLException {
+
+        String sql = "CREATE TABLE IF NOT EXISTS Users (ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR(30), LASTNAME VARCHAR(30), AGE INT);";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropUsersTable() throws SQLException {
+
+        String sql = "DROP TABLE IF EXISTS Users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+
+        String sql = "INSERT INTO Users (NAME, LASTNAME, AGE) VALUES(?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeUserById(long id) throws SQLException {
+
+        String sql = "DELETE FROM Users WHERE ID=?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() throws SQLException {
+        List<User> usersList = new ArrayList<>();
+
+        String sql = "SELECT ID, NAME, LASTNAME, AGE FROM Users";
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setLastName(resultSet.getString("LASTNAME"));
+                user.setAge(resultSet.getByte("AGE"));
+                usersList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usersList;
+    }
+
+    public void cleanUsersTable() throws SQLException {
+
+        String sql = "TRUNCATE TABLE Users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
